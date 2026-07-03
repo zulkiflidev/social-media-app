d0d337e37856a59a79bfa11f394cdbfd48b4d0b9
zulkiflidev <aganzulkifli@gmail.com>
2026-07-03 16:36:05 +0700

    feat: add useComments hook

---
+++ features/comments/hooks/useComments.ts
@@ -0,0 +1,42 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import api from "@/lib/api/axios";
+
+import type { CommentsResponse } from "@/features/comments/types/comments";
+
+
+function useComments(postId: number) {
+
+
+    return({
+        queryKey: ["comments", postId],
+        queryFn: async ({ pageParam }:  { pageParam: number }) => {
+
+            const response = await api.get<{
+                data: CommentsResponse;
+            
+            }>( `/posts/${postId}/comments`, {
+                params: {
+                    page: pageParam,
+                    limit: 10
+            
+                },
+            });
+
+            return response.data.data;
+        },
+        initialPageParam: 1,
+        getNextPageParam: (lastPage: CommentsResponse) => {
+
+            const { page, totalPages } = lastPage.pagination;
+            return page < totalPages ? page + 1 : undefined;
+
+        },
+        enabled: !!postId,
+
+
+
+    });
+
+}
+
+export default useComments;
